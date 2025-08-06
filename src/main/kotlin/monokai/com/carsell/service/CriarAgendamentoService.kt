b34d03a73326562d34ea6a49aa0f69f25d17d408
jeoliveira045 <jean.mateus.1997@gmail.com>
2025-08-06 18:24:18 -0300

    Implementação do AgendamentoService

--- src/main/kotlin/monokai/com/carsell/service/CriarAgendamentoService.kt
+++ src/main/kotlin/monokai/com/carsell/service/CriarAgendamentoService.kt
@@ -3,6 +3,7 @@ package monokai.com.carsell.service
 import monokai.com.carsell.domain.model.Agendamento
 import monokai.com.carsell.repositories.AgendamentoRepository
 import org.springframework.stereotype.Service
+import java.time.LocalTime
 
 @Service
 class CriarAgendamentoService(
@@ -11,9 +12,13 @@ class CriarAgendamentoService(
 
     fun exec(agendamento: Agendamento): Agendamento {
         val carroAgendadoList = agendamentoRepository.findAgendamentoByCarro_Id(agendamento.carro.id!!)
-        carroAgendadoList.map {agendamento1 -> agendamento1.dataHora}.forEach{ data ->
-            if(data == agendamento.dataHora) throw RuntimeException("O carro já está agendado para este horário. Verifique outro horário.")
-        }
+        carroAgendadoList.map {agendamento1 -> agendamento1.dataHora}.forEach{ data -> if(data == agendamento.dataHora) throw RuntimeException("O carro já está agendado para este horário. Verifique outro horário.") }
+        val agendamentoCliente = carroAgendadoList.find { agendamento2 -> agendamento2.dataHora == agendamento.dataHora }!!
+        if(agendamento.cliente.id == agendamentoCliente.id) throw RuntimeException("O cliente já realizou agendamento nesse horario!")
+        val time = agendamento.dataHora.toLocalTime()
+        val foraExpediente = time.isAfter(LocalTime.of(18,0)) || time.isBefore(LocalTime.of(8,0))
+        if(foraExpediente)throw RuntimeException("O estabelecimento está fechado no momento. Escolha outro horario")
+
         return agendamentoRepository.save(agendamento)
     }
 }
