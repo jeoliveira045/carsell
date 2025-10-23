18f55e1a039a14816cb82e66255b06a66a5041c5
jeoliveira045 <jean.mateus.1997@gmail.com>
2025-10-23 13:59:49 -0300

    Criação da classe RelatorioDataMesView e seu respectivo crud

--- src/main/kotlin/monokai/com/carsell/domain/model/vw/RelatorioDataMesView.kt
+++ src/main/kotlin/monokai/com/carsell/domain/model/vw/RelatorioDataMesView.kt
@@ -5,27 +5,29 @@ import jakarta.persistence.Id
 import org.hibernate.annotations.Subselect
 
 @Entity
-@Subselect("""
-    SELECT
-    ROWNUM AS ID,
-    CASE EXTRACT(MONTH FROM DATA_CRIACAO)
-        WHEN 1 THEN 'Janeiro'
-        WHEN 2 THEN 'Fevereiro'
-        WHEN 3 THEN 'Março'
-        WHEN 4 THEN 'Abril'
-        WHEN 5 THEN 'Maio'
-        WHEN 6 THEN 'Junho'
-        WHEN 7 THEN 'Julho'
-        WHEN 8 THEN 'Agosto'
-        WHEN 9 THEN 'Setembro'
-        WHEN 10 THEN 'Outubro'
-        WHEN 11 THEN 'Novembro'
-        WHEN 12 THEN 'Dezembro'
-        END AS MES,
-    COUNT(*) AS QTD_VENDAS
+@Subselect("""SELECT month_number as id, MES, QTD_VENDAS FROM (
+SELECT EXTRACT(MONTH FROM DATA_CRIACAO) as month_number, CASE EXTRACT(MONTH FROM DATA_CRIACAO)
+WHEN 1 THEN 'Janeiro'
+WHEN 2 THEN 'Fevereiro'
+WHEN 3 THEN 'Março'
+WHEN 4 THEN 'Abril'
+WHEN 5 THEN 'Maio'
+WHEN 6 THEN 'Junho'
+WHEN 7 THEN 'Julho'
+WHEN 8 THEN 'Agosto'
+WHEN 9 THEN 'Setembro'
+WHEN 10 THEN 'Outubro'
+WHEN 11 THEN 'Novembro'
+WHEN 12 THEN 'Dezembro'
+END  AS MES,
+COUNT(*) AS QTD_VENDAS
 FROM VENDAS
 GROUP BY EXTRACT(MONTH FROM DATA_CRIACAO)
 ORDER BY EXTRACT(MONTH FROM DATA_CRIACAO) ASC
+) 
+GROUP BY MES, QTD_VENDAS, month_number
+ORDER BY id
+
 """)
 data class RelatorioDataMesView(
     @Id
